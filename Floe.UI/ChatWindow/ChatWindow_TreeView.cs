f86c3f60fb9a831e87c04ee4aad86a4bfcc47453
Gergo F <gergo_f@yahoo.com>
2017-02-26 19:00:33 +0000

    Properly update New Server Tab's Networkname on connection

--- Floe.UI/ChatWindow/ChatWindow_TreeView.cs
+++ Floe.UI/ChatWindow/ChatWindow_TreeView.cs
@@ -5,32 +5,61 @@ using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using Floe.Net;
 
 namespace Floe.UI
 {
-    public class NetworkTreeViewItem
-    {
-        public ChatPage Page { get; private set; }
-        public string NetworkName { get { return Page.Header == null ? "Not loaded" : Page.Header; } }
+	public class NetworkTreeViewItem : INotifyPropertyChanged
+	{
+		public ChatPage Page { get; private set; }
+		public string NetworkName
+		{
+			get
+			{
+				return Page.Header == null ? "Not loaded" : Page.Header;
+			}
+			private set
+			{
+				OnPropertyChanged("NetworkName");
+			}
+		}
 
-        public NetworkTreeViewItem(ChatPage page)
-        {
-            this.Page = page;
-            ChannelItems = new ObservableCollection<ChannelTreeViewItem>();
-        }
+		public NetworkTreeViewItem(ChatPage page)
+		{
+			this.Page = page;
+			this.Page.Session.StateChanged += Session_StateChanged;
+			ChannelItems = new ObservableCollection<ChannelTreeViewItem>();
+		}
 
-        public ObservableCollection<ChannelTreeViewItem> ChannelItems { get; set; }
-    }
+		private void Session_StateChanged(object sender, EventArgs e)
+		{
+			this.NetworkName = Page.Header;
+		}
 
-    public class ChannelTreeViewItem
-    {
-        public ChatPage Page { get; private set; }
-        public string ChannelName { get { return Page.Target.Name; } }
+		public ObservableCollection<ChannelTreeViewItem> ChannelItems { get; set; }
 
-        public ChannelTreeViewItem(ChatPage page)
-        {
-            this.Page = page;
-        }
-    }
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged(string name)
+		{
+			var handler = this.PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(name));
+			}
+		}
+
+	}
+
+	public class ChannelTreeViewItem
+	{
+		public ChatPage Page { get; private set; }
+		public string ChannelName { get { return Page.Target.Name; } }
+
+		public ChannelTreeViewItem(ChatPage page)
+		{
+			this.Page = page;
+		}
+	}
 }
