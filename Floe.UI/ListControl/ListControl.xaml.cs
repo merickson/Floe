80b5a6228fad34710d7dad9ef4b7b0b6cc736ba8
Seven-7 <gergo_f@yahoo.com>
2018-01-27 23:56:47 +0000

    The right way for sorting /LIST channels

--- Floe.UI/ListControl/ListControl.xaml.cs
+++ Floe.UI/ListControl/ListControl.xaml.cs
@@ -81,8 +81,8 @@ namespace Floe.UI
 				case IrcCode.RPL_LISTEND:
 					this.IsCloseable = true;
 					this.Session.InfoReceived -= new EventHandler<IrcInfoEventArgs>(Session_InfoReceived);
-                    _channels = _channels.OrderByDescending(c => c.Count).OrderBy(n => n.Name).ToList();
-					foreach (var c in _channels)
+                    _channels = _channels.OrderByDescending(c => c.Count).ThenBy(n => n.Name).ToList();
+                    foreach (var c in _channels)
 					{
 						lstChannels.Items.Add(c);
 					}
